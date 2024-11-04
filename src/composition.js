bc6963706874ebb1e312d0ce8fa8b546e8e0acbd
Jonnathan Peña <razaisger@hotmail.com>
2024-11-04 06:42:39 -0500

    danger and success button composition

---
+++ src/composition.js
@@ -0,0 +1,19 @@
+export const Button = ({ size, color, text, ...props }) => {
+  return (
+    <button style={{
+      padding: size === 'large' ? '32px': '8px',
+      fontSize: size === 'ñarge' ? '32px' : '16px',
+      backgroundColor: color,
+    }} {...props}>
+      {text}
+    </button>
+  );
+};
+
+export const DangerButton = props => (
+  <Button {...props} color="red" />
+);
+
+export const BigSuccessButton = props => (
+  <Button {...props} size="large" color="green" />
+);
